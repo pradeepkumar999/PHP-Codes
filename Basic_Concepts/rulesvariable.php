bfaf3f126f4942dc87be5c5e6d164d76d829cb7b
pradeep <pradeepvirat96@gmail.com>
2023-09-08 12:27:21 +0530

    Variables

--- Basic_Concepts/rulesvariable.php
+++ Basic_Concepts/rulesvariable.php
@@ -2,6 +2,6 @@
 echo "1.starts with dollar<br>";
 echo "2.cannot start with a number <br>";
 echo "3.Must start with letter or underscore <br>";
-echo "4.Can only contain alphanumeric Characters and underscore";
+echo "4.Can only contain alphanumeric Characters and underscore <br>";
 echo "5.variables are case sensitive";
 ?>
