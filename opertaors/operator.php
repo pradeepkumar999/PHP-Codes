ebcd9754a5a5c29d88ba35b4c4e50daced992868
pradeep <pradeepvirat96@gmail.com>
2023-09-08 16:45:01 +0530

    operators

---
+++ opertaors/operator.php
@@ -0,0 +1,23 @@
+<?php
+/*operators in PHP
+1.Arthimetic operator
+2.Assignment operator
+3.Comparison operator
+4.Logical operator
+*/
+
+$a=45;
+$b=8;
+echo "1.Arthimetic operator<br>";
+echo "for a + b ".($a+$b)."<br>";
+echo "for a - b ".($a-$b)."<br>";
+echo "for a * b ".($a*$b)."<br>";
+echo "for a / b ".($a/$b)."<br>";
+echo "for a % b ".($a/$b)."<br>";
+echo "for a ** b ".($a**$b)."<br>";
+
+echo "2.Assignment operator<br>";
+$x=$a;
+echo "For x,the value is". $x."<br>";
+
+?>
