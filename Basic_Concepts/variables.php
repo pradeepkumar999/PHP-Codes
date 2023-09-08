bd22f3175259b834399f5691a5848aff6bb45ca4
pradeep <pradeepvirat96@gmail.com>
2023-09-08 12:05:20 +0530

    Variables

---
+++ Basic_Concepts/variables.php
@@ -0,0 +1,17 @@
+<?php
+
+//echo "This is Tutorial";
+//variables are containers for storing information
+//keywords are not case sensitive
+//variables are case sensitive
+
+$name="pradeep";
+$income=200;
+
+echo "This guy is $name and his income is $income <br>";
+
+echo "$name is a good boy <br>";
+echo "$name is the real gangsta <br>";
+
+
+?>
