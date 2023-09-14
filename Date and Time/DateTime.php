ef78c7d614caf21624830e6d631855d93c4a334e
pradeep <pradeepvirat96@gmail.com>
2023-09-14 19:15:16 +0530

    date and time

---
+++ Date and Time/DateTime.php
@@ -0,0 +1,27 @@
+<?php
+
+//strtotime
+
+echo strtotime(datetime: '26 aug 2022');
+
+//timestamp
+echo strtotime('today');
+
+//date
+echo date('Y-m-d',strtotime('yesterday'));
+
+echo date('Y-m-d');
+
+//dateformats
+
+echo date('j');
+echo date('D');
+echo date('l');
+
+echo date('H:i:s T');
+
+
+
+
+
+?>
